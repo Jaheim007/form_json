dac8404a406d2476ae37a43f4c197ae0ef08b242
iMac-13 <imac-13@iMac-de-iMac-13.local>
2022-01-14 12:46:45 +0000

    form

---
+++ res.py
@@ -0,0 +1,14 @@
+# Style de titre
+title = {
+     "text": "Formulaire D'Inscription", 
+    "bg":"orange red",
+    "fg":"white",
+    "font":"Roboto 25",
+
+}
+
+global_style = {
+    "font": "Verdana 20", 
+    "bg": "orange red", 
+    "fg": "white"
+}
--- workspace.py
+++ workspace.py
@@ -1,34 +1,36 @@
-from ast import Return
 from lib2to3.pgen2.grammar import Grammar
+from multiprocessing.dummy import Value
 from tkinter import*
 from tkinter import font
-import json
+from tkinter import messagebox
+import res
 
 #Creating the Size of the Form and title
 base = Tk()
-base.title("Inscription")
+base.title("Page D'Inscription")
 base.geometry("600x550")
+base.eval('tk::PlaceWindow . center')
 base.resizable(False,False)
-base.configure(bg="steel blue")
+base.configure(bg="orange red")
 
 #Creating the Label First Name, Last Name, Email and Contact
-Label(base, text= "Formulaire d'inscription",bg="steel blue" ,font="Roboto 25", fg="white").pack(pady=50)
-Label(base,text= "Nom", font= "arial 20", bg="steel blue", fg="white" ).place(x=45, y=150)
-Label(base, text="Prenom", font="arial 20", bg="steel blue", fg="white" ).place(x=45, y=200)
-Label(base, text="Email", font="arial 20" , bg="steel blue", fg="white").place(x=45, y=250)
-Label(base, text="Telephone", font="arial 20", bg="steel blue", fg="white").place(x=45, y=300)
+Label(base, **res.title).pack(pady=50)
+Label(base,text= "Nom", **res.global_style).place(x=45, y=150)
+Label(base, text="Prenom", **res.global_style).place(x=45, y=200)
+Label(base, text="Email", **res.global_style).place(x=45, y=250)
+Label(base, text="Telephone", **res.global_style).place(x=45, y=300)
 
 #Stating that the Values are all string
-nomValue = StringVar()
-prenomValue = StringVar()
-emailValue = StringVar()
-telephoneValue = StringVar()
+nomEntry = StringVar()
+prenomEntry = StringVar()
+emailEntry = StringVar()
+telephoneEntry = IntVar()
 
 #Creating the entry of the Form
-nomEntry = Entry(base, textvariable=nomValue ,width=30, bd=2, font="arial 20")
-prenomEntry = Entry(base, textvariable=prenomValue ,width=30, bd=2, font="arial 20")
-emailEntry = Entry(base, textvariable=emailValue ,width=30, bd=2, font="arial 20")
-telephoneEntry = Entry(base, textvariable=telephoneValue ,width=30, bd=2, font="arial 20")
+nomEntry = Entry(base, textvariable=nomEntry ,width=30, bd=2, font="arial 20")
+prenomEntry = Entry(base, textvariable=prenomEntry ,width=30, bd=2, font="arial 20")
+emailEntry = Entry(base, textvariable=emailEntry ,width=30, bd=2, font="arial 20")
+telephoneEntry = Entry(base, textvariable=telephoneEntry ,width=30, bd=2, font="arial 20")
 
 #Placing the entry of the Form
 nomEntry.place(x=150, y=150)
@@ -39,7 +41,19 @@ telephoneEntry.place(x=150, y=300)
 #Creating the JSON file to store all the information
 def show_entry():
     f = open("name.json", "a")
-    f.write("Nom: %s\nPrenom: %s\nEmail: %s\nTelephone: %s\n" % (nomEntry.get(), prenomEntry.get(), emailEntry.get(), telephoneEntry.get()))
+    f.write("Nom : %s\nPrenom : %s\nEmail : %s\nTelephone : %s\n" % (nomEntry.get(), prenomEntry.get(), emailEntry.get(), telephoneEntry.get()))
+    
+    #Creating sereval variables to obtain the input of the user 
+    nom = nomEntry.get()
+    prenom = prenomEntry.get()
+    email = emailEntry.get()
+    telephone = telephoneEntry.get()
+    
+    #Creating the Condition of the message box which is empty
+    if nom == "" or prenom =="" or email =="" or telephone =="":
+        messagebox.showerror("Error", "Veuillez saisir vos informations.")
+    
+    #Deleteing the Entry
     nomEntry.delete(0,END)
     prenomEntry.delete(0,END)
     emailEntry.delete(0,END)
@@ -47,6 +61,6 @@ def show_entry():
     f.close()
 
 #Creating the button
-Button(base ,text="Valider",command=show_entry, fg="black", bg="spring green", font = "arial 20", width =11, height=2).place(x=200, y=360)
+Button(base ,text="Valider",command=show_entry, fg="black", bg="spring green", font = "arial 18", width =10, height=2).place(x=250, y=360)
 
 base.mainloop()
